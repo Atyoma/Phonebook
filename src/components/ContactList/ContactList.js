7468f9e03acce87c494a4ead53e461e8767ef511
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-06-03 16:08:10 +0300

    fixed contactList

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import ContactItem from 'components/ContactItem';
 import Loader from 'components/Loader';
 import s from './ContactList.module.css';
-
+import Filter from '../../components/Filter';
 function ContactList() {
   const { data: contacts, isFetching } = useFetchContactsQuery();
   const filter = useSelector(state => state.filter.value);
@@ -23,16 +23,22 @@ function ContactList() {
   return (
     <>
       {isFetching && <Loader />}
-      {contacts ? (
-        <ul className={s.list}>
-          {filteredContacts.map(contact => (
-            <li key={contact.id}>
-              <ContactItem contact={contact} />
-            </li>
-          ))}
-        </ul>
+      {!isFetching && contacts.length > 0 ? (
+        <section className={s.contactsBox}>
+          <h2 className={s.title}>Contacts</h2>
+
+          <Filter />
+
+          <ul className={s.list}>
+            {filteredContacts.map(contact => (
+              <li key={contact.id}>
+                <ContactItem contact={contact} />
+              </li>
+            ))}
+          </ul>
+        </section>
       ) : (
-        <p>No any contacts</p>
+        <p className={s.title}>No any contacts</p>
       )}
     </>
   );
--- src/pages/ContactsPage/Contactspage.js
+++ src/pages/ContactsPage/Contactspage.js
@@ -1,6 +1,6 @@
 import Form from '../../components/Form';
 import ContactList from '../../components/ContactList';
-import Filter from '../../components/Filter';
+
 import s from '../../components/ContactList/ContactList.module.css';
 
 function ContactsPage() {
@@ -8,13 +8,7 @@ function ContactsPage() {
     <div>
       <h1 className={s.title}>Phonebook</h1>
       <Form />
-      <h2 className={s.title}>Contacts</h2>
-      <div className={s.filterBox}>
-        <Filter />
-      </div>
-      <div className={s.contactsBox}>
-        <ContactList />
-      </div>
+      <ContactList />
     </div>
   );
 }
