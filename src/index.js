d9d7b96d03c9fa60174a82029487607ab3bdb088
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-10-11 16:58:18 +0200

    change baseName

--- src/index.js
+++ src/index.js
@@ -8,19 +8,17 @@ import './index.css';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>      
-        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
-           <App />  
-        </BrowserRouter> 
-      </PersistGate>  
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/phonebook/">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 reportWebVitals();
